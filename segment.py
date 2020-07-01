3847ec4c2ddd981f5191799d690726c82eaf068e
David Knapp <david.knapp1@gmx.de>
2020-07-01 21:52:54 +0200

    Added background changing

--- segment.py
+++ segment.py
@@ -26,6 +26,8 @@ def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_res
                    [0, 255, 255], [85, 255, 255], [170, 255, 255]]
 
     im = np.array(im)
+    bground = cv2.imread('bg_bggenerator_com.jpg', cv2.IMREAD_COLOR)
+    bground = np.array(bground)
     vis_im = im.copy().astype(np.uint8)
     vis_parsing_anno = parsing_anno.copy().astype(np.uint8)
     vis_parsing_anno = cv2.resize(vis_parsing_anno, None, fx=stride, fy=stride, interpolation=cv2.INTER_NEAREST)
@@ -33,9 +35,9 @@ def vis_parsing_maps(im, parsing_anno, stride, save_im=False, save_path='vis_res
 
     num_of_class = np.max(vis_parsing_anno)
 
-    for pi in range(1, num_of_class + 1):
-        index = np.where(vis_parsing_anno == pi)
-        vis_parsing_anno_color[index[0], index[1], :] = part_colors[pi]
+    
+    index = np.where(vis_parsing_anno == 0)
+    vis_im[index[0], index[1], :] = bground[index[0], index[1]]
 
     vis_parsing_anno_color = vis_parsing_anno_color.astype(np.uint8)
     # print(vis_parsing_anno_color.shape, vis_im.shape)
@@ -86,5 +88,3 @@ def evaluate(respth='./res/test_res', dspth='./data', cp='model_final_diss.pth')
 
 if __name__ == "__main__":
     evaluate(dspth='F:\CelebAMask-HQ\CelebAMask-HQ\CelebA-HQ-img', cp='79999_iter.pth')
-
-
